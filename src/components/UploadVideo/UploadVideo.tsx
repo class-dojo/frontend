d233c01a672248715b090aa8703ebe35eb8fe148
Filippo Ceron <kaim.aragonar@gmail.com>
2022-05-08 05:02:37 +0200

    feat: implement send image file to s3 bucket

--- src/components/UploadVideo/UploadVideo.tsx
+++ src/components/UploadVideo/UploadVideo.tsx
@@ -7,37 +7,61 @@ const UploadVideo = () => {
   const [source, setSource] = useState<string | Buffer | Blob | File>('');
   const [message, setMessage] = useState('Click the button to transcode');
   const [frames, setFrames] = useState<string[]>([]);
+
+  // set the array frames inside an object as value
+
   const ffmpeg = createFFmpeg({
     log: true,
   });
-  const doTranscode = async () => {
+
+  const handleTranscodeClick = async () => {
     setMessage('Loading ffmpeg-core.js');
     await ffmpeg.load();
     setMessage('Start transcoding');
     ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(source));
-    await ffmpeg.run('-i', 'test.mp4', '-vf', 'fps=1/5', 'out%d.png');
+    await ffmpeg.run('-i', 'test.mp4', '-vf', 'fps=1/5', '%d.png');
     setMessage('Complete transcoding');
     const frameArray = [];
     for (let i = 1; i<=14; i++) {
-      const frame = ffmpeg.FS('readFile', `out${i}.png`);
+      const frame = ffmpeg.FS('readFile', `${i}.png`);
       frameArray.push(frame);
     }
 
-    frameArray.forEach(frame => setFrames(prev => {
-      const blobString = URL.createObjectURL(new Blob([frame], { type: 'image/png' }));
-      // console.log(blobString);
-      // const sliced = blobString.slice(5);
-      const frameUrls = [...prev, blobString];
-      //console.log(sliced);
-      return frameUrls;
-    }));
+    const filesArray: File[] = [];
+    frameArray.forEach((frame, i) =>
+    {
+      setFrames(prev => {
+        const blobString = URL.createObjectURL(new Blob([frame], { type: 'image/png' }));
+        const frameUrls = [...prev, blobString];
+        return frameUrls;
+      });
+      const blobFile = new File([frame], `${i+1}.png`);
+      filesArray.push(blobFile);
+    });
 
-    // await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
-    // setMessage('Transcoding Complete');
-    // const data = ffmpeg.FS('readFile', 'out.gif');
-    // console.log(data);
-    // setFrames(URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' })));
+    // TODO upload to bucket
+    // const DataToBeSent = {
+    //   [source.toString()]: filesArray
+    // };
+
+    const url = 'http://images.localhost:9000/images/dummy.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=root%2F20220507%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220507T123814Z&X-Amz-Expires=604800&X-Amz-Signature=627218060ca95a97b5b0b8413ac00e4eeaf72c7adb746a0b3346e7d59665b807&X-Amz-SignedHeaders=host';
+    // take the first file from FilesList
+    const file = filesArray[0];
+    // Extract Content-Type & filename
+    const {type} = file;
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': type,
+      },
+      body: file,
+    })
+      .then(console.log)
+      .catch(console.error);
+
   };
+
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
@@ -60,7 +84,7 @@ const UploadVideo = () => {
           </select>
           <div>
             <input type="file" accept="video/*" onChange={handleChange}/>
-            <div className="my-3"><a className="btn btn-primary btn-lg me-2" role="button" onClick={doTranscode}>UPLOAD VIDEO</a></div>
+            <div className="my-3"><a className="btn btn-primary btn-lg me-2" role="button" onClick={handleTranscodeClick}>UPLOAD VIDEO</a></div>
             <p>{message}</p>
             <sub>Estimated duration: 3 min</sub>
           </div>
