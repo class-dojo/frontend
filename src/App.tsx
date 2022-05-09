08968ecad6dc0a7488b9fb656ebdad2c44f59fb2
Jack Blakeston <jackblakeston93@gmail.com>
2022-05-09 05:50:42 +0200

    minor: rename folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Navbar from './components/Navbar/Navbar';
 import UploadVideo from './components/UploadVideo/UploadVideo';
-import AttentionChart from './components/InterestChart/AttentionChart';
+import AttentionChart from './components/AttentionChart/AttentionChart';
 
 function App () {
   return (
