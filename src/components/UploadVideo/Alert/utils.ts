1bd0729fd772333d5e133bc3c45e244142e189b3
Filippo Ceron <kaim.aragonar@gmail.com>
2022-05-16 11:33:53 +0200

    dev: add warning message for failed analysis

--- src/components/UploadVideo/Alert/utils.ts
+++ src/components/UploadVideo/Alert/utils.ts
@@ -1,5 +1,6 @@
 import { AlertMessageProps } from '../types';
 
 export const fileNotSelected: AlertMessageProps = { heading: 'Forgot anything?', body: 'Please select a video to analyze', variant: 'danger' };
-export const loaderNotReady: AlertMessageProps = { heading: 'Hold down, cowboy!', body: 'The loader is not not ready yet, please wait for the "Start transcoding" message to appear', variant: 'danger' };
+//export const loaderNotReady: AlertMessageProps = { heading: 'Hold down, cowboy!', body: 'The loader is not not ready yet, please wait for the "Start transcoding" message to appear', variant: 'danger' };
 export const uploadSuccessful: AlertMessageProps = { heading: 'Success!', body: 'The video analysis is ready for you to check', variant: 'success' };
+export const analysisError: AlertMessageProps = { heading: 'Ooops, something went wrong!', body: 'Video analysis unsuccessful, please try again', variant: 'danger' };
